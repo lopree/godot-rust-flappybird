7344492d508cff05b36e66bdbec90784cc8bee3a
RookStein <zhutong1993@163.com>
2024-11-26 17:58:09 +0800

    slider

---
+++ bird-rust/src/ui/music_slider.rs
@@ -0,0 +1,29 @@
+use godot::{classes::{Slider, ISlider}, prelude::*};
+
+#[derive(GodotClass)]
+#[class(base=Slider)]
+struct MusicSlider{
+    base:Base<Slider>
+}
+
+#[godot_api]
+impl ISlider for MusicSlider{
+    fn init(base:Base<Slider>)->Self{
+        Self{base}
+    }
+    fn enter_tree(&mut self){
+        self.base_mut().set_min(0.0);
+        self.base_mut().set_max(1.0);
+        self.base_mut().set_value(1.0);
+        let callable = Callable::from_object_method(&self.base(), "on_slider_value_changed");
+        self.base_mut().connect("value_changed", &callable);
+    }
+}
+
+#[godot_api]
+impl MusicSlider{
+    #[func]
+    fn on_slider_value_changed(&mut self, value:f32){
+        godot_print!("slider_value:{}",value);
+    }
+}
